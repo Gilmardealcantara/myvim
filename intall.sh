ba44d584402cb0b96cdc15f1c510028d329899cc
Gilmar Alcantara <gilmardealcantara@gmail.com>
2017-09-30 18:15:33 -0300

    Add intall you complete clang

--- intall.sh
+++ intall.sh
@@ -1,5 +1,12 @@
 sudo apt install vim
+sudo apt install ctags
 cp .vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
+# install glang complete
+cd ~/.vim/bundle
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe
+git submodule update --init --recursive
+./install.sh --clang-completer
